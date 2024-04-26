45eb974d33199b13b430a0d37dedab6ccf4a6113
Vojtěch Věchet <115136203+vojtavechet@users.noreply.github.com>
2024-04-26 15:46:16 +0200

    final main.js

--- main.js
+++ main.js
@@ -1,26 +1,29 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow } = require('electron')
+const path = require('node:path')
+ 
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 800
-  })
 
-  win.loadFile('index.html')
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon:"icon.png",
+ })
+  
+  
+
+  mainWindow.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
   createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
 })
-
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+ 
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
 })
+ 
+ 
